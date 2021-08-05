eb7e0ab2372fa6f326d05b199f891dae9ac2031e
timlautk <timlautk@gmail.com>
2021-08-05 11:22:56 -0500

    edit plot_metrics.py

--- seac/plot_metrics.py
+++ seac/plot_metrics.py
@@ -3,17 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # import json
 
+from absl import app
+from absl import flags
 
-df = pd.read_json("~/Desktop/2021 Zebra Research Intern/GitHub/seac/seac/results/sacred/001/metrics.json")
-# print(df)
+FLAGS = flags.FLAGS
 
-
-df_agent0 = df["agent0/episode_reward"]
-# print(df_agent0)
-plt.plot(df_agent0["steps"], df_agent0["values"])
-plt.show()
+flags.DEFINE_string("path", "results/sacred/1", "path of the metrics json file")
 
 
-df_agent1 = df["agent1/episode_reward"]
-plt.plot(df_agent1["steps"], df_agent1["values"])
-plt.show()
+def main(_):
+    path = FLAGS.path
+    df = pd.read_json(path + "/metrics.json")
+    # print(df)
+
+
+    df_agent0 = df["agent0/episode_reward"]
+    # print(df_agent0)
+    plt.plot(df_agent0["steps"], df_agent0["values"])
+    plt.show()
+
+
+    df_agent1 = df["agent1/episode_reward"]
+    plt.plot(df_agent1["steps"], df_agent1["values"])
+    plt.show()
+
+
+if __name__ == "__main__":
+    app.run(main)
