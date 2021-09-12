de98f3d5c8438f74b128a1914ede8435d4e1de28
tl1567 <85906996+tl1567@users.noreply.github.com>
2021-09-12 17:05:01 -0500

    Update simulations_small-5ag.sh

--- seac/simulations_small-5ag.sh
+++ seac/simulations_small-5ag.sh
@@ -3,6 +3,6 @@
 
 for ((i=0; i<=99; i++))
 do
-    python3 evaluate_cbs_rware_replanning.py --env_name=rware-small-5ag-v1 --seed=i
+    python3 evaluate_cbs_rware_replanning.py --env_name=rware-small-5ag-v1 --seed=$i
     echo "Iteration $i finished"
 done
