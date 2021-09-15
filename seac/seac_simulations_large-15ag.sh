6cb8725abea7c2bc9f9cd7d30be93de5c5e0d6df
tl1567 <85906996+tl1567@users.noreply.github.com>
2021-09-15 11:24:15 -0500

    Create seac_simulations_large-15ag.sh

---
+++ seac/seac_simulations_large-15ag.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+for ((i=0; i<=99; i++))
+do
+    python3 evaluate_trained.py --env_name=rware-large-15ag-v1 --path=results/trained_models/103/u218000 --seed=$i
+    echo "Iteration $i finished"
+done
