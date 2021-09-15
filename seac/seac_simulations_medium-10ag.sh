d02dadc6d7cb70ae70656d9d3cb63aa136ba31d3
tl1567 <85906996+tl1567@users.noreply.github.com>
2021-09-15 11:15:29 -0500

    Create seac_simulations_medium-10ag.sh

---
+++ seac/seac_simulations_medium-10ag.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+for ((i=0; i<=99; i++))
+do
+    python3 evaluate_trained.py --env_name=rware-medium-10ag-v1 --path=results/trained_models/99/u240000 --seed=$i
+    echo "Iteration $i finished"
+done
