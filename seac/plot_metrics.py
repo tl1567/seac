b3ecf880c2d66571c15a1dce91a8feb058cb19fe
tl1567 <85906996+tl1567@users.noreply.github.com>
2021-09-15 19:19:26 -0500

    Update plot_metrics.py

--- seac/plot_metrics.py
+++ seac/plot_metrics.py
@@ -4,8 +4,6 @@ import pandas as pd
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = 'serif'
-plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 
 from absl import app
 from absl import flags
