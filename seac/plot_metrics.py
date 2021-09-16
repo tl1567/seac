1c739b639b88700a8efd165ae2615302b26d4f3e
tl1567 <85906996+tl1567@users.noreply.github.com>
2021-09-15 19:18:20 -0500

    Update plot_metrics.py

--- seac/plot_metrics.py
+++ seac/plot_metrics.py
@@ -4,7 +4,8 @@ import pandas as pd
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 
 from absl import app
 from absl import flags
@@ -34,6 +35,7 @@ def main(_):
     plt.plot(steps, sum_df_agents)
     plt.title(f"Sum of rewards of all agents")
     plt.show()
+    plt.savefig(f"{path}/total_reward.pdf")
 
     # df_agent2 = df["agent2/episode_reward"]
     # plt.plot(df_agent2["steps"], df_agent2["values"])
